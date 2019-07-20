9d4eafe0e871e33b42bf0de5330326be8f9019dc
Khalyomede <khalyomede@gmail.com>
2019-07-20 18:22:35 +0200

    added icon component

---
+++ src/components/MaterializeIcon.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+    i.material-icons {{ icon }}
+</template>
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
