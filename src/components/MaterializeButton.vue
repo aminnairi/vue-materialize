828e42d148feb7112f98cd8d690875623e389071
Khalyomede <khalyomede@gmail.com>
2019-07-20 19:23:21 +0200

    removed icon from the button

--- src/components/MaterializeButton.vue
+++ src/components/MaterializeButton.vue
@@ -1,28 +1,8 @@
 <template>
   <button type="button" class="waves-effect waves-light btn">
-    <i v-if="icon" class="classes">
-      {{ icon }}
-      <slot />
-    </i>
+    <slot />
   </button>
 </template>
 <script>
-export default {
-  props: {
-    icon: {
-      type: String,
-      default: ""
-    },
-    iconSide: {
-      type: String,
-      default: "left",
-      validator: value => ["left", "right"].includes(value)
-    }
-  },
-  computed: {
-    classes() {
-      return `material-icons ${iconSide}`;
-    }
-  }
-};
+export default {};
 </script>
