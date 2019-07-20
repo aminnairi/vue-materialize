a388267bd50f61d216ae5afc02cd9d46ea5113c1
Khalyomede <khalyomede@gmail.com>
2019-07-20 18:32:51 +0200

    added component breadcrumb item

---
+++ src/components/MaterializeBreadcrumbItem.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    div
+        router-link.breadcrumb(v-if="router")
+            slot
+        a.breadcrumb(v-else)
+            slot
+</template>
+<script>
+export default {
+  props: {
+    router: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
