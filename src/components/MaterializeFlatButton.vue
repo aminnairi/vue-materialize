d23d4bf96222d41e041a76a559d4258cd3d6fb15
Khalyomede <khalyomede@gmail.com>
2019-07-20 18:02:21 +0200

    removed icons from the component

--- src/components/MaterializeFlatButton.vue
+++ src/components/MaterializeFlatButton.vue
@@ -1,25 +1,7 @@
 <template lang="pug">
     button.waves.waves-light.btn-flat(type="button")
-        i(v-if="icon" :class="classes")
         slot
 </template>
 <script>
-export default {
-  props: {
-    icon: {
-      type: String,
-      default: ""
-    },
-    iconSide: {
-      type: String,
-      default: "left",
-      validator: value => ["left", "right"].includes(value)
-    }
-  },
-  computed: {
-    classes() {
-      return `material-icons ${this.iconSide}`;
-    }
-  }
-};
+export default {};
 </script>
