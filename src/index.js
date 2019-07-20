062bf4675c285c77c4e704660d41ae62477feca3
Amin NAIRI <nairi.amin@gmail.com>
2019-07-20 19:19:38 +0200

    :sparkles: exporting the material icon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import MaterializeBrandLogo from "./components/MaterializeBrandLogo.vue";
+import MaterializeIcon from "./components/MaterializeIcon.vue";
 
 export default {
     install(Vue) {
         Vue.component("materialize-brand-logo", MaterializeBrandLogo);
+        Vue.component("materialize-icon", MaterializeIcon);
     }
 };
