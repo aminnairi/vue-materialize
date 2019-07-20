f9ace8757d77e049ed385a2d6448cf0a35b43896
Khalyomede <khalyomede@gmail.com>
2019-07-20 21:11:54 +0200

    added fixed navigation to the documentation

--- docs/docs.js
+++ docs/docs.js
@@ -13,18 +13,22 @@ window.addEventListener("load", () => {
         data: {
             components: [
                 {
+                    slug: "icon",
                     code: '<materialize-icon icon="face" />',
                     name: "Icon"
                 },
                 {
+                    slug: "brand-logo",
                     code: "<materialize-brand-logo>Application Name</materialize-brand-logo>",
                     name: "Brand Logo"
                 },
                 {
+                    slug: "breadcrumb",
                     code: "<materialize-breadcrumb>\n    <!-- breadcrumb items here -->\n</materialize-breadcrumb>",
                     name: "Breadcrumb"
                 },
                 {
+                    slug: "breadcrumb-item",
                     code: '<materialize-breadcrumb-item>Home</materialize-breadcrumb-item>\n<materialize-breadcrumb-item to="products">Products</materialize-breadcrumb-item>',
                     name: "Breadcrumb Item"
                 }
@@ -35,6 +39,8 @@ window.addEventListener("load", () => {
         mounted() {
             // eslint-disable-next-line
             Prism.highlightAll();
+            M.ScrollSpy.init(document.querySelectorAll(".scrollspy"));
+            M.Pushpin.init(document.querySelector("#fixed-navigation"));
         }
     });
 });
