b1262fe3c87fe384e22e36aa983438cac28649d0
Khalyomede <khalyomede@gmail.com>
2019-07-20 17:35:32 +0200

    added raised button

---
+++ src/components/MaterializeRaisedButton.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    button.waves-effect.waves-light.btn(type="button")
+        i(v-if="icon" v-bind:class="`material-icons ${iconSide}`"")
+            {{ icon }}
+        slot
+</template>
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: ""
+    },
+    iconSide: {
+      type: String,
+      default: "left",
+      validator: value => ["left", "right"].includes(value)
+    }
+  }
+};
+</script>
