f7b6315f9c5d8c95a4497ecc6dd78168c3549b12
Khalyomede <khalyomede@gmail.com>
2019-07-20 17:58:10 +0200

    added flat button component

---
+++ src/components/MaterializeFlatButton.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+    button.waves.waves-light.btn-flat(type="button")
+        i(v-if="icon" :class="classes")
+        slot
+</template>
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: ""
+    },
+    iconSide: {
+      type: String,
+      default: "left",
+      validator: value => ["left", "right"].includes(value)
+    }
+  },
+  computed: {
+    classes() {
+      return `material-icons ${this.iconSide}`;
+    }
+  }
+};
+</script>
