b009d10918aadfc31e561c6fdd2a0d2d0c27c0e5
Amin NAIRI <nairi.amin@gmail.com>
2019-07-21 19:02:56 +0200

    :sparkles: added all components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
+import MaterializeChips from "./components/MaterialChips.vue";
 import MaterializeBrandLogo from "./components/MaterializeBrandLogo.vue";
 import MaterializeBreadcrumb from "./components/MaterializeBreadcrumb.vue";
 import MaterializeBreadcrumbItem from "./components/MaterializeBreadcrumbItem.vue";
 import MaterializeButton from "./components/MaterializeButton.vue";
+import MaterializeCheckbox from "./components/MaterializeCheckbox.vue";
 import MaterializeColumn from "./components/MaterializeColumn.vue";
+import MaterializeContainer from "./components/MaterializeContainer.vue";
 import MaterializeIcon from "./components/MaterializeIcon.vue";
+import MaterializeNavbar from "./components/MaterializeNavbar.vue";
+import MaterializeRadio from "./components/MaterializeRadio.vue";
+import MaterializeRow from "./components/MaterializeRow.vue";
+import MaterializeSwitch from "./components/MaterializeSwitch.vue";
 
 export default {
     install(Vue) {
-        Vue.component("materialize-brand-logo", MaterializeBrandLogo);
-        Vue.component("materialize-icon", MaterializeIcon);
-        Vue.component("materialize-button", MaterializeButton);
-        Vue.component("materialize-breadcrumb", MaterializeBreadcrumb);
-        Vue.component("materialize-breadcrumb-item", MaterializeBreadcrumbItem);
-        Vue.component("materialize-column", MaterializeColumn);
+			Vue.component("materialize-chips", MaterializeChips);
+			Vue.component("materialize-brand-logo", MaterializeBrandLogo);
+			Vue.component("materialize-breadcrumb", MaterializeBreadcrumb);
+			Vue.component("materialize-breadcrumb-item", MaterializeBreadcrumbItem);
+			Vue.component("materialize-button", MaterializeButton);
+			Vue.component("materialize-checkbox", MaterializeCheckbox);
+			Vue.component("materialize-column", MaterializeColumn);
+			Vue.component("materialize-container", MaterializeContainer);
+			Vue.component("materialize-icon", MaterializeIcon);
+			Vue.component("materialize-navbar", MaterializeNavbar);
+			Vue.component("materialize-radio", MaterializeRadio);
+			Vue.component("materialize-row", MaterializeRow);
+			Vue.component("materialize-switch", MaterializeSwitch);
     }
 };
