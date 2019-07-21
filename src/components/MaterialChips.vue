d08acb00a8ea5237b9170df11213564058ed7102
Khalyomede <khalyomede@gmail.com>
2019-07-21 12:32:39 +0200

    add component chips

---
+++ src/components/MaterialChips.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="chip">
+        <img v-if="image" :src="image" :alt="alt" />
+        <slot />
+        <i v-if="closable" class="material-icons">close</i>
+  </div>
+</template>
+<script>
+export default {
+    props: {
+        image: {
+            type: String,
+            default: ""
+        },
+        alt: {
+            type: String,
+            required: this.image
+        },
+        closable: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
