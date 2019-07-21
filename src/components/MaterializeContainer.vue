0fbb81260a318a16308ef86a0bbfa16c92a09368
Amin NAIRI <nairi.amin@gmail.com>
2019-07-21 19:03:09 +0200

    :tada: initial commit

---
+++ src/components/MaterializeContainer.vue
@@ -0,0 +1,25 @@
+<template>
+	<div :class="classes">
+		<slot />
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		full: {
+			type: Boolean,
+			default: false
+		}
+	},
+
+	computed: {
+		classes() {
+			return {
+				"container": !this.full,
+				"container-full": this.full
+			};
+		}
+	}
+};
+</script>
