0aa68a118889d28af828fa6a4bc426938c8346e5
Khalyomede <khalyomede@gmail.com>
2019-07-27 12:55:53 +0200

    added component collection

---
+++ src/components/MaterializeCollection.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul :class="classes">
+        <slot />
+    </ul>
+</template>
+<script>
+export default {
+    props: {
+        withHeader: {
+            type: Boolean,
+            default: false
+        }
+    },
+    computed: {
+        classes() {
+            return {
+                "collection": true,
+                "with-header": this.withHeader
+            }
+        }
+    }
+}
+</script>
